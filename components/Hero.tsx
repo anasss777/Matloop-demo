772612484ec98f0056fb06bb01cc2c111bf58744
anasss777 <anass2000271@gmail.com>
2024-02-10 11:35:32 +0300

    Hero section animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,21 +1,60 @@
+"use client";
+
+import {
+  fedeInAnimationVariants1,
+  fedeInAnimationVariants2,
+  fedeInAnimationVariants3,
+  fedeInAnimationVariants4,
+} from "@/utils/animations";
+import { motion } from "framer-motion";
 import React from "react";
 
 const Hero = () => {
   return (
     <div>
-      <div className="w-full md:w-1/2 px-8 pb-5 pt-10 bg-teal-500 rounded-ee-[100px]">
+      <div className="w-full px-12 pb-5 pt-10 bg-teal-500 rounded-ee-[100px] border-t border-teal-200">
         <h1 className="text-5xl font-bold text-teal-200">
-          جد مطلوبك
-          <span className="flex my-5 justify-center">او</span>
-          <span className="flex justify-end">قدم ما لديك</span>
+          <motion.span
+            variants={fedeInAnimationVariants1}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            className="flex justify-start"
+          >
+            جد مطلوبك
+          </motion.span>
+          <motion.span
+            variants={fedeInAnimationVariants3}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            className="flex my-3 justify-center text-4xl"
+          >
+            او
+          </motion.span>
+          <motion.span
+            variants={fedeInAnimationVariants2}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            className="flex justify-end"
+          >
+            قدم ما لديك
+          </motion.span>
         </h1>
-        <p className="text-lg mt-12 mb-10 text-teal-700 pl-5">
+        <motion.p
+          variants={fedeInAnimationVariants4}
+          initial="initial"
+          whileInView="animate"
+          viewport={{ once: true }}
+          className="text-lg mt-12 mb-10 text-teal-700 pl-5"
+        >
           تواصل مع المستخدمين الباحثين عن{" "}
           <span className="text-teal-100 font-semibold">موظفين</span> او{" "}
           <span className="text-teal-100 font-semibold">سيارات</span> او{" "}
           <span className="text-teal-100 font-semibold">عقارات</span> او{" "}
           <span className="text-teal-100 font-semibold">أجهزة إلكترونية</span>
-        </p>
+        </motion.p>
       </div>
     </div>
   );
---
+++ utils/animations.ts
@@ -0,0 +1,60 @@
+const fedeInAnimationVariants1 = {
+    initial: {
+        opacity: 0,
+        x: -100,
+    },
+    animate: () => ({
+        opacity: 1,
+        x: 0,
+        transition: {
+        ease: "linear",
+        duration: 1,
+        delay: 1,
+        },
+    }),
+};
+const fedeInAnimationVariants2 = {
+    initial: {
+        opacity: 0,
+        x: 100,
+    },
+    animate: () => ({
+        opacity: 1,
+        x: 0,
+        transition: {
+        ease: "linear",
+        duration: 1,
+        delay: 1,
+        },
+    }),
+};
+
+const fedeInAnimationVariants3 = {
+    initial: {
+        opacity: 0,
+    },
+    animate: () => ({
+        opacity: 1,
+        transition: {
+        ease: "linear",
+        duration: 1.5,
+        delay: 1.5,
+        },
+    }),
+};
+
+const fedeInAnimationVariants4 = {
+    initial: {
+        scale: 0,
+    },
+    animate: () => ({
+        scale: 1,
+        transition: {
+        ease: "linear",
+        duration: 0.5,
+        delay: 1.5,
+        },
+    }),
+};
+
+export {fedeInAnimationVariants1, fedeInAnimationVariants2, fedeInAnimationVariants3, fedeInAnimationVariants4}
