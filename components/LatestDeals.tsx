4d0c3bc66c617a5fc12950512ff2fb383c463f79
anasss777 <anass2000271@gmail.com>
2024-03-11 15:16:22 +0300

    Fixed Error in LatestDeals

--- components/LatestDeals.tsx
+++ components/LatestDeals.tsx
@@ -1,16 +1,44 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import PostCard from "./PostCard";
 import Link from "next/link";
+import { CarPost } from "@/types/post";
+import firebase from "@/firebase";
 
 const LatestDeals = () => {
+  const [posts, setPosts] = useState<CarPost[]>();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const querySnapshot = await firebase
+        .firestore()
+        .collection("carsPosts")
+        .get();
+      const PostsData: CarPost[] = querySnapshot.docs.map(
+        (doc) =>
+          ({
+            ...doc.data(),
+          } as CarPost)
+      );
+      setPosts(PostsData);
+    };
+
+    fetchPosts();
+  }, []);
   return (
     <div className="flex flex-col w-full h-fit pb-20 pt-10 px-5">
       <h1 className="text-xl mt-5 font-semibold text-[#4682b4] flex flex-row justify-between">
         أحدث الصفقات
       </h1>
-      <PostCard />
-      <PostCard allowImg />
-      <PostCard />
+      {posts?.map((post, index) => (
+        <PostCard
+          key={index}
+          posterName={post.poster.name}
+          posterImage={post.poster.profileImageSrc}
+          post={post}
+        />
+      ))}
 
       <Link
         href="/"
